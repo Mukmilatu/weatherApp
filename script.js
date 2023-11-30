ae70de2a2d51e01b9eb059fa933a06c549eeecbf
Mukmilatu <muqmilat@gmail.com>
2023-11-30 15:17:03 +0100

    added something

--- script.js
+++ script.js
@@ -6,23 +6,31 @@ let today = `${months[date.getMonth()]}`;
 console.log(date.getUTCFullYear())
 console.log(today);
 
-async function getWeather(city) {
+async function getWeather(userName, city) {
     let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0f2d99b0ba5f9b1b176aabd3634d0b32`);
     let data = await res.json();
-    console.log(data.weather[0].description);
+    
+    document.getElementById('weatherInfo').textContent = data.weather[0].main +  " " + Math.round(data.main.temp) + "°C";
+    let temp =  document.createElement('p').textContent;
+    temp =  data.weather[0].description;
+    document.querySelector('.weatherInfo').append(temp)
+    // hum = data.weather[0].main;
+
+    document.getElementById('userInfo').textContent= "Welcome! " + userName;
+    
+
+   console.log(data)
 }
 
-let  promise = new  Promise()
+// let  promise = new  Promise()
 
-
+getWeather('Mukmilat', 'Lagos')
 
 submitBtn.addEventListener('click', () => {
-    let userName = document.getElementById("userName").value; 
-    let location = document.getElementById("location").value;
-    let userInfo  = document.querySelector('.userInfo')
-    let weatherInfo = document.querySelector('.weatherInfo');
-    weatherInfo.textContent = "Welcome to  " + location;
-    userInfo.textContent = "Welcome Oh! you " + userName;
+    
+    let userName = document.getElementById('userName').value; 
+    let location = document.getElementById('location').value; 
+    getWeather(userName, location)
     
 
  
